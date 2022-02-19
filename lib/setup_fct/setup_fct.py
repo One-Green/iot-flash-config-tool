dcdac5e5a15663cc09c570b2b77f217f3ef81e4e
Shanmugathas Vigneswaran <vigneswaran.shanmugathas@flowbird.group>
2022-02-19 21:21:33 +0100

    feat(project): add inner class for handler, add logger, use shutil

--- lib/setup_fct/setup_fct.py
+++ lib/setup_fct/setup_fct.py
@@ -1,20 +1,50 @@
-import subprocess
+import shutil
+import platform
+import colorlog
 
+logger = colorlog.getLogger("default-logger")
 
-def __binary_probe(_bin: str) -> bool:
-    try:
-        subprocess.call(
-            [_bin],
-            stderr=subprocess.DEVNULL,
-            stdout=subprocess.DEVNULL)
-        return True
-    except FileNotFoundError:
-        return False
+GIT: str = "git"
+PYTHON: str = "python"
+PIO: str = "pio"
+PIO_EXE: str = "pio.exe"
 
 
 def is_git_exist() -> bool:
-    return __binary_probe("git")
+    _ = shutil.which(GIT)
+    if _:
+        logger.info(f"{GIT} found: '{_}'")
+        return True
+    else:
+        return False
 
 
 def is_python_exist() -> bool:
-    return __binary_probe("python")
+    _ = shutil.which(PYTHON)
+    if _:
+        logger.info(f"{PYTHON} found: '{_}'")
+        return True
+    else:
+        return False
+
+
+def is_pio_exist() -> bool:
+    _platform = platform.system()
+
+    if _platform == ("Linux" or "Darwin"):
+        _ = shutil.which(PIO)
+        if _:
+            logger.info(f"{PIO} for {_platform} found: '{_}'")
+            return True
+        else:
+            return False
+    elif platform.system() == "Windows":
+        _ = shutil.which(PIO)
+        if _:
+            logger.info(f"{PIO} for {_platform} found: '{_}'")
+            return True
+        else:
+            return False
+    else:
+        logger.error(f"This platform '{_platform}' is not handled by this tool")
+        return False
--- main.py
+++ main.py
@@ -2,6 +2,13 @@ import sys
 from PyQt6.QtWidgets import QMainWindow, QApplication
 from lib.main_ui import Ui_MainWindow
 from lib.setup_fct import setup_fct
+import colorlog
+
+handler = colorlog.StreamHandler()
+handler.setFormatter(colorlog.ColoredFormatter('%(log_color)s%(levelname)s:%(name)s:%(message)s'))
+logger = colorlog.getLogger('default-logger')
+logger.addHandler(handler)
+logger.setLevel(colorlog.DEBUG)
 
 
 class Main(QMainWindow):
@@ -9,23 +16,62 @@ class Main(QMainWindow):
         super(Main, self).__init__()
 
         # build ui
+        logger.debug("loading ui")
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        logger.debug("ui loaded")
 
-        # button method link
-        self.ui.pushButton.clicked.connect(self.check_git)
+        # handler
+        logger.debug("loading handlers classes")
+        self.setup_tab = self.InnerSetupTabHandler(ui=self.ui)
+        logger.debug("loading handlers classes: done")
 
-    def check_git(self):
-        if setup_fct.is_git_exist():
-            self.ui.label_7.setText("OK")
-            self.ui.label_7.setStyleSheet("background-color: green;")
-        else:
-            self.ui.label_7.setText("KO")
-            self.ui.label_7.setStyleSheet("background-color: red;")
+        # --- button method link
+        # ----- tab: setup
+        logger.debug("linking button to actions")
+        self.ui.gitCheckButton.clicked.connect(self.setup_tab.check_git)
+        self.ui.gitInstallButton.setEnabled(False)
+        # ----- Python
+        self.ui.pythonCheckButton.clicked.connect(self.setup_tab.check_python)
+        self.ui.pythonInstallButton.setEnabled(False)
+        # ----- Platform IO
+        self.ui.pioCheckButton.clicked.connect(self.setup_tab.check_pio)
+        self.ui.pioInstallButton.setEnabled(False)
+        logger.debug("linking button to actions done")
+
+    class InnerSetupTabHandler:
+        def __init__(self, ui):
+            self.ui = ui
+
+        def check_git(self):
+            if setup_fct.is_git_exist():
+                self.ui.gitStatusLabel.setText("OK")
+                self.ui.gitStatusLabel.setStyleSheet("background-color: green;")
+            else:
+                self.ui.gitStatusLabel.setText("KO")
+                self.ui.gitStatusLabel.setStyleSheet("background-color: red;")
+                self.ui.gitInstallButton.setEnabled(True)
+
+        def check_python(self):
+            if setup_fct.is_python_exist():
+                self.ui.pythonStatusLabel.setText("OK")
+                self.ui.pythonStatusLabel.setStyleSheet("background-color: green;")
+            else:
+                self.ui.pythonStatusLabel.setText("KO")
+                self.ui.pythonStatusLabel.setStyleSheet("background-color: red;")
+                self.ui.pythonInstallButton.setEnabled(True)
+
+        def check_pio(self):
+            if setup_fct.is_pio_exist():
+                self.ui.pioStatusLabel.setText("OK")
+                self.ui.pioStatusLabel.setStyleSheet("background-color: green;")
+            else:
+                self.ui.pioStatusLabel.setText("KO")
+                self.ui.pioStatusLabel.setStyleSheet("background-color: red;")
+                self.ui.pioInstallButton.setEnabled(True)
 
 
 if __name__ == '__main__':
-
     app = QApplication(sys.argv)
     main = Main()
     main.show()
