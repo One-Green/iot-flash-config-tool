85224787c1c1b445a76eff4373467b75494f4b8f
Shanmugathas Vigneswaran <vigneswaran.shanmugathas@flowbird.group>
2022-02-22 21:44:54 +0100

    feat(src.handlers.firmware_def): hardening platform / nodes definition

--- src/handlers/firmware_def.py
+++ src/handlers/firmware_def.py
@@ -1,23 +1,36 @@
 # related to this project
 # https://github.com/One-Green/iot-edge-agent
 
+from dataclasses import dataclass
+
+
+@dataclass
+class Platform:
+    esp_32: str = "esp32"
+    arduino_nano: str = "arduino-nano"
+    arduino_mega: str = "arduino-mega"
+
 
 class BaseMicroController(object):
-    def __init__(self, caption_name, platform, src_path):
-        self.name: str = caption_name
+    def __init__(self, name, platform, src_path):
+        self.name: str = name
         self.platform: str = platform
         self.src_path: str = src_path
 
+    @property
+    def caption_name(self):
+        return f"{self.name}-{self.platform}"
+
 
 SprinklerNodes = [
-    BaseMicroController(caption_name="default", platform="esp32", src_path="sprinkler")
+    BaseMicroController(name="default", platform=Platform.esp_32, src_path="sprinkler")
 ]
 
 WaterTankNodes = [
     BaseMicroController(
-        caption_name="default", platform="esp32", src_path="water/master_esp32"
+        name="default", platform=Platform.esp_32, src_path="water/master_esp32"
     ),
     BaseMicroController(
-        caption_name="mega-slave-1", platform="esp32", src_path="water/i2c_slave_mega"
+        name="slave-1", platform=Platform.arduino_mega, src_path="water/i2c_slave_mega"
     ),
 ]
