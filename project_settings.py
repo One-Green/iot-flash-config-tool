33f8422f505419a56364b7c8b286108b8d9a9d32
Shanmugathas Vigneswaran <vigneswaran.shanmugathas@flowbird.group>
2022-02-24 22:26:56 +0100

    feat(project_settings.py): add pio_logger with file file handler

--- project_settings.py
+++ project_settings.py
@@ -1,5 +1,6 @@
 import os
 import colorlog
+import logging
 
 LOCAL_REPO: str = os.path.join(os.getcwd(), "_firmware")
 
@@ -10,3 +11,8 @@ handler.setFormatter(
 logger = colorlog.getLogger("default-logger")
 logger.addHandler(handler)
 logger.setLevel(colorlog.DEBUG)
+
+pio_logger = colorlog.getLogger("pio-logger")
+pio_logger.addHandler(handler)
+pio_logger.addHandler(logging.FileHandler("pio.log"))
+pio_logger.setLevel(colorlog.DEBUG)
